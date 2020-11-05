cf61314eeb3d75ae024623057a0ee606a6fb6b7a
fartem <jaman.smlnsk@gmail.com>
2020-11-05 19:32:27 +0300

    2020-11-05 Update: Added "Find the unique number"

---
+++ src/main/java/com/smlnskgmail/jaman/codewarsjava/kyu6/FindTheUniqueNumber.java
@@ -0,0 +1,30 @@
+package com.smlnskgmail.jaman.codewarsjava.kyu6;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+public class FindTheUniqueNumber {
+
+    private final double[] input;
+
+    public FindTheUniqueNumber(double[] input) {
+        this.input = input;
+    }
+
+    public double solution() {
+        Map<Double, Integer> uniqueNumbers = new HashMap<>();
+        for (double number: input) {
+            uniqueNumbers.merge(number, 1, Integer::sum);
+        }
+
+        double uniqueNumber = 0.0;
+        for (Map.Entry<Double, Integer> entry: uniqueNumbers.entrySet()) {
+            if (entry.getValue() == 1) {
+                uniqueNumber = entry.getKey();
+            }
+        }
+        return uniqueNumber;
+    }
+
+}
---
+++ src/test/java/com/smlnskgmail/jaman/codewarsjava/kyu6/FindTheUniqueNumberTest.java
@@ -0,0 +1,23 @@
+package com.smlnskgmail.jaman.codewarsjava.kyu6;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindTheUniqueNumberTest {
+
+    @Test
+    public void sampleTestCases() {
+        assertEquals(
+                1.0,
+                new FindTheUniqueNumber(new double[] { 0, 1, 0 }).solution(),
+                0.0000000000001
+        );
+        assertEquals(
+                2.0,
+                new FindTheUniqueNumber(new double[] { 1, 1, 1, 2, 1, 1 }).solution(),
+                0.0000000000001
+        );
+    }
+
+}
